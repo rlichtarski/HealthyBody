06986de88ce93b1b023cd207b45662e1be3338da
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-10-20 01:57:25 +0200

    app: add a registration fragment layout

---
+++ app/app/src/main/java/com/hackheroes/healthybody/ui/auth/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.hackheroes.healthybody.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hackheroes.healthybody.R
+
+class AuthActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+
+
+}
