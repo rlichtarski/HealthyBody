e67f9d72d5243d8b30249c74fd4822728961f93e
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-10-23 19:51:55 +0200

    app: fix previous commit - add graph layout and fragment, remake dashboard

--- app/app/src/main/java/com/hackheroes/healthybody/ui/main/GraphFragment.kt
+++ app/app/src/main/java/com/hackheroes/healthybody/ui/main/GraphFragment.kt
@@ -1,2 +1,55 @@
 package com.hackheroes.healthybody.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.hackheroes.healthybody.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class GraphFragment : Fragment() {
+
+    @Inject
+    lateinit var mAuth: FirebaseAuth
+
+    private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var userId: String
+
+    val personCollectionRef = Firebase.firestore.collection("users")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_graph, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userId = mAuth.currentUser?.uid!!
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        meal_icon.setOnClickListener {
+            //findNavController().navigate(R.id.action_dashboardFragment_to_mealsFragment)
+        }
+
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+    }
+
+}
