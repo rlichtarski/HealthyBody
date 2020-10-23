cd58364530f49b290bb78b3a3570cfd2a7c5c3f4
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-10-24 01:43:09 +0200

    app: basic room setup

---
+++ app/app/src/main/java/com/hackheroes/healthybody/db/RunningDatabase.kt
@@ -0,0 +1,13 @@
+package com.hackheroes.healthybody.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
+}
